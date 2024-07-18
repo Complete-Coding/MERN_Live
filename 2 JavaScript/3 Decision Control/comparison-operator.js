8fa6ffff01ce258d991df04ce4dbfc059302693d
Prashant Jain <prashant@knowledgegate.in>
2024-07-18 21:33:04 +0530

    update

---
+++ 2 JavaScript/3 Decision Control/comparison-operator.js
@@ -0,0 +1,12 @@
+console.log('Learning about comparison operators in JavaScript');
+console.log(5 === 5);
+
+
+console.log('Program start');
+let age = 24;
+if (age >= 18) { 
+  console.log('You are an adult');
+} else {
+  console.log('You are a minor');
+}
+console.log('Program end');
