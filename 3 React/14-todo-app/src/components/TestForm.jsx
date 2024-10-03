9926f4407c1110e612320a0c95b715abd374799f
Prashant Jain <prashant@knowledgegate.in>
2024-10-03 21:08:54 +0530

    update

--- 3 React/14-todo-app/src/components/TestForm.jsx
+++ 3 React/14-todo-app/src/components/TestForm.jsx
@@ -1,33 +1,20 @@
 import React from 'react'
-import { useState } from 'react';
 import { useRef } from 'react';
 
 const TestForm = () => {
   console.log("Painting Form");
 
-  const [firstNameVal, setFirstName] = useState();
-
-  const clicks = useRef(0);
   const firstName = useRef();
   const lastName = useRef();
   const dob = useRef();
 
-  const firstNameChangeHandler = (event) => {
-    setFirstName(event.target.value);
-  }
-
   const submitHandler = (event) => {
     event.preventDefault();
     console.log("Button Clicked");
-    // console.log(firstName.current.value);
-    // console.log(lastName.current.value);
-    // console.log(dob.current.value);
+    console.log(firstName.current.value);
+    console.log(lastName.current.value);
+    console.log(dob.current.value);
   }
-
-  // const handleOnChange = (event) => {
-  //   firstName.current = event.target.value;
-  //   console.log(firstName.current);
-  // }
 
   return (
     <div>
@@ -37,7 +24,7 @@ const TestForm = () => {
         <input type="text" ref={lastName} placeholder='Last Name' name="lastName"/><br />
         <label htmlFor="dob" > DOB </label>
         <input id="dob" ref={dob} type="date" name="dob"/><br />
-        <input type="submit" onClick={() => {clicks.current += 1 }}/>
+        <input type="submit"/>
       </form>
 
     </div>
