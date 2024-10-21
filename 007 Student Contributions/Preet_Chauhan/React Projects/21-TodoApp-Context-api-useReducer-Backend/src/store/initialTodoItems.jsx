ef35aabe76f3d5366f35d109367e8e4ba8a8750d
Preets-Version-77331 <preetchauhan76747@gmail.com>
2024-10-21 15:19:49 +0530

    ADD TODO PROJECT

---
+++ 007 Student Contributions/Preet_Chauhan/React Projects/21-TodoApp-Context-api-useReducer-Backend/src/store/initialTodoItems.jsx
@@ -0,0 +1,45 @@
+// fetch('http://localhost:3000/todos')
+// .then(res => {
+//     if (!res.ok) {
+//         throw new Error('Todos not Found in server ');
+
+//     }
+//     return res.json();
+
+// })
+
+// .then(console.log )
+
+
+
+// const intialTodoItem = [ 
+//     { id: 1, todoText: 'Buy Milk ', todoDate: '04-08-2024' },
+//     { id: 2, todoText: 'Evening walk ', todoDate: '06-08-2024' },
+//     { id: 3, todoText: 'Go Market', todoDate: '20-08-2024' },
+// ];
+
+// export default intialTodoItem;  
+
+
+
+
+
+
+// const initialTodoItem = 
+
+// fetch('http://localhost:3000/todos')
+//   .then(res => {
+//     if (!res.ok) {
+//       throw new Error('Todos not found in server');
+//     }
+//     return res.json(); // Convert response to JSON
+//   })
+//   .then(data => {
+//     console.log('Fetched todos:', data); // Successfully fetched data
+//   })
+//   .catch(error => {
+//     console.error(error.message); // Handle the error and use initialTodoItem
+//     console.log('Falling back to initial todo items:', initialTodoItem);
+//   });
+
+// export default initialTodoItem;
