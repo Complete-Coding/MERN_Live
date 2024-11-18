6a2f6eef5ccb3115c100a09618bba814860d1a45
Prashant Jain <prashant@knowledgegate.in>
2024-11-18 21:05:20 +0530

    Class 82

--- 5 NodeJs and ExpressJs/15 authentication/controllers/authController.js
+++ 5 NodeJs and ExpressJs/15 authentication/controllers/authController.js
@@ -2,9 +2,18 @@ exports.getLogin = (req, res, next) => {
   res.render('auth/login', {pageTitle: 'Login', isLoggedIn: false});
 }
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {pageTitle: 'Login', isLoggedIn: false});
+}
+
 exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true;
   res.redirect("/");
+}
+
+exports.postSignup = (req, res, next) => {
+  console.log('User came for signup: ', req.body);
+  res.redirect("/login");
 }
 
 exports.postLogout = (req, res, next) => {
--- 5 NodeJs and ExpressJs/15 authentication/routers/authRouter.js
+++ 5 NodeJs and ExpressJs/15 authentication/routers/authRouter.js
@@ -5,5 +5,7 @@ const authController = require("../controllers/authController");
 authRouter.get("/login", authController.getLogin);
 authRouter.post("/login", authController.postLogin);
 authRouter.post("/logout", authController.postLogout);
+authRouter.get("/signup", authController.getSignup);
+authRouter.post("/signup", authController.postSignup);
 
 exports.authRouter = authRouter;
